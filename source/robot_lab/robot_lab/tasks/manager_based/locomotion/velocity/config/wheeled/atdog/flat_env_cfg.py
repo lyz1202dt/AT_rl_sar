b9fee405b56eab3ebca00a9ef810e35ef78ba770
lyz1202dt <cloud1202@qq.com>
2026-04-28 20:51:34 +0800

    增加机器人ATDogDog（轮足机器人）

---
+++ source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/config/wheeled/atdog/flat_env_cfg.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2024-2025 Ziqi Fan
+# SPDX-License-Identifier: Apache-2.0
+
+from isaaclab.utils import configclass
+
+from .rough_env_cfg import ATDogDogRoughEnvCfg
+
+
+@configclass
+class ATDogDogFlatEnvCfg(ATDogDogRoughEnvCfg):
+    def __post_init__(self):
+        # post init of parent
+        super().__post_init__()
+
+
+        self.events.randomize_reset_base.params["pose_range"] = {
+            "x": (-0.03, 0.03),
+            "y": (-0.03, 0.03),
+            "z": (0.0, 0.02),
+            "roll": (-0.05, 0.05),
+            "pitch": (-0.05, 0.05),
+            "yaw": (-0.1, 0.1),
+        }
+        self.events.randomize_reset_base.params["velocity_range"] = {
+            "x": (0.0, 0.0),
+            "y": (0.0, 0.0),
+            "z": (0.0, 0.0),
+            "roll": (0.0, 0.0),
+            "pitch": (0.0, 0.0),
+            "yaw": (0.0, 0.0),
+        }
+
+        # override rewards
+        self.rewards.base_height_l2.params["sensor_cfg"] = None
+        # change terrain to flat
+        self.scene.terrain.terrain_type = "plane"
+        self.scene.terrain.terrain_generator = None
+        # no height scan
+        self.scene.height_scanner = None
+        self.observations.policy.height_scan = None
+        self.observations.critic.height_scan = None
+        # no terrain curriculum
+        self.curriculum.terrain_levels = None
+
+        # If the weight of rewards is 0, set rewards to None
+        if self.__class__.__name__ == "ATDogDogFlatEnvCfg":
+            self.disable_zero_weight_rewards()
